73e406992259e2e2503cdfd7962cd3c9ed6c7683
Bartłomiej Żabicki <bartlomiej.zabicki@untitledkingdom.com>
2020-05-16 18:42:53 +0200

    Remove crashDoctor and KSCrash

--- Sources/CrasheeObjc/include/BridgeHeader.h
+++ Sources/CrasheeObjc/include/BridgeHeader.h
@@ -7,30 +7,7 @@
 //That's the header which imports necessary data for .swift files
 
 #import <Foundation/Foundation.h>
-//#include "../Recording/KSCrash.h"
 #import "../Reporting/Filters/KSCrashReportFilterBasic.h"
-#import "../Recording/KSCrash.h"
-//#import "../Reporting/Tools/KSCString.h"
-//#import "../Recording/Tools/KSJSONCodecObjC.h"
-//#import "../Recording/Tools/KSLogger.h"
-//#import "../Recording/Tools/NSError+SimpleConstructor.h"
-//
-//#import "../Reporting/Tools/KSHTTPMultipartPostBody.h"
-//#import "../Reporting/Tools/KSHTTPRequestSender.h"
-//#import "../Reporting/Filters/Tools/NSData+GZip.h"
-//#import "../Recording/Tools/KSJSONCodecObjC.h"
-//#import "../Reporting/Tools/KSReachabilityKSCrash.h"
-//#import "../Recording/Tools/NSError+SimpleConstructor.h"
-//#import "../Reporting/Filters/KSCrashReportFilterAppleFmt.h"
-//#import "../Reporting/Filters/KSCrashReportFilterBasic.h"
-//#import "../Reporting/Filters/KSCrashReportFilterGZip.h"
 
 #import "../Recording/KSCrashC.h"
-#import "../Recording/KSCrashDoctor.h"
-//#import "../Recording/KSCrashReportFields.h"
-//#import "../Recording/Monitors/KSCrashMonitor_AppState.h"
-//#import "../Recording/Tools/KSJSONCodecObjC.h"
-//#import "../Recording/Tools/NSError+SimpleConstructor.h"
 #import "../Recording/Monitors/KSCrashMonitorContext.h"
-//#import "../Recording/Monitors/KSCrashMonitor_System.h"
-//#import "../Recording/KSSystemCapabilities.h"
