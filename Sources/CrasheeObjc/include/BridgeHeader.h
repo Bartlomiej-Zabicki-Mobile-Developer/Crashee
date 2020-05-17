7c31ba7e2e4b70d86a05e185e3929d5c86efb1ec
Bartłomiej Żabicki <bartlomiej.zabicki@untitledkingdom.com>
2020-05-17 11:50:04 +0200

    Remove filters,nserror and jsonCodec

--- Sources/CrasheeObjc/include/BridgeHeader.h
+++ Sources/CrasheeObjc/include/BridgeHeader.h
@@ -7,7 +7,7 @@
 //That's the header which imports necessary data for .swift files
 
 #import <Foundation/Foundation.h>
-#import "../Reporting/Filters/KSCrashReportFilterBasic.h"
+//#import "../Reporting/Filters/KSCrashReportFilterBasic.h"
 
 #import "../Recording/KSCrashC.h"
 #import "../Recording/Monitors/KSCrashMonitorContext.h"
